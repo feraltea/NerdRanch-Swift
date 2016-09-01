f6ecf8a85d198796d01edfa7135e2da064707b66
Ty Frith <tyfrith@gmail.com>
2016-09-01 16:42:35 -0700

    protocol start

--- Protocols.playground/Contents.swift
+++ Protocols.playground/Contents.swift
@@ -4,25 +4,4 @@ import UIKit
 
 var str = "Hello, playground"
 
-func printTable(data: [[Int]]) {
-    for row in data {
-        //create an empty string 
-        var out = ""
-        
-    for item in row {
-        //append each item in this row to our string 
-        out += " \(item) |"
-        }
-        
-        //Done - print it!
-        print(out)
-    }
-}
-let data = [
-    [30, 6],
-    [1, 8],
-    [50, 20]
-]
-
-printTable(data)
 
