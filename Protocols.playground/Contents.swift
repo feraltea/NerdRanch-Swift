976c9ce766a585e5f40070938beb2a514b4d7546
Ty Frith <tyfrith@gmail.com>
2016-10-24 19:06:05 -0700

    protocols again

--- Protocols.playground/Contents.swift
+++ Protocols.playground/Contents.swift
@@ -2,56 +2,30 @@
 
 import UIKit
 
-var str = "Hello, playground"
+//var str = "Hello, playground"
+//
+//print(str)
 
-//create helper function to add padding
-
-func padding(amount: Int) -> String {
-    var paddingString = ""
-    for _ in 0 ..< amount {
-        paddingString += " "
-    }
-    return paddingString
-}
-
-
-// func to take array of arrays and prints numbers in a table
-
-func printTable(rowLabels: [String], data: [[Int]]) {
-    
-    //create an array of the width of each row label
-    let rowLabelWidths = rowLabels.map { $0.characters.count }
-    
-    //determine length of longest row label 
-    guard let maxRowLabelWidth = rowLabelWidths.maxElement() else {
-        return
-    }
-    
-    for (i, row) in data.enumerate() {
-        //pad the tow label out so they are all the same length
-        let paddingAmount = maxRowLabelWidth - rowLabelWidths[i]
-        var out = rowLabels[i] + padding(paddingAmount) + " |"
-  
-    
-    //append each item in this row to our string
+func printTable(data: [[Int]]) {
+    for row in data {
+        //create an empty string 
+        var out = ""
+        
+        //append each item in this row to our string 
         for item in row {
             out += " \(item) |"
         }
-        
-        //done, now print it
+        //done, now print 
         print(out)
     }
 }
 
-
-let rowLabels = ["Joe", "Tim", "Ragnar"]
 let data = [
-    [30, 6],
-    [40, 18],
-    [50, 20]
+[30, 6],
+[40, 2],
+[50, 3],
 ]
 
-printTable(rowLabels, data: data)
-
+printTable(data: data)
 
 
